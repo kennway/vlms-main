7da7075b18fc8ec1ac421a1f71f79bb934dc5f2a
kennway <76895112+kennway@users.noreply.github.com>
2021-12-07 20:19:28 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/index.css">
+        <title>Welcome to Visitor's Log Monitoring System</title>
+    </head>
+    <body>
+        <div class = "title-content">
+            <img src="images/bupc-logo.png" alt="">
+            <div class = "center-title">
+                <h1>Bicol University</h1>
+                <h2>Polangui Campus</h2>
+                <h3>Visitor's Log Monitoring System</h3>
+            </div>
+            <img src="images/bu-main-logo.png" alt="">
+        </div>
+        <div class = "login-content">
+            <form action="index.html">
+                <div class = "inputs-tags">
+                    <h4>Enter Email Address</h4>
+                    <input type="text" name="input-email" required>
+                </div>
+                <div class = "inputs-tags">
+                    <h4>Enter Password</h4>
+                    <input type="password" name="input-passcode" required>
+                </div>
+                <div class = "login-btn">
+                    <button>Login</button>
+                </div>
+
+            </form>
+        </div>
+    </body>
+</html>
